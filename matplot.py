8778e4b710c55073e34fead00f9f01a01a1a7c0b
Atif <at454507@dal.ca>
2019-02-10 08:26:18 +0000

    These are the files that will be required to run the programs successfully.

---
+++ matplot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as pyplot
+def mat():
+	pyplot.clf()
+	#pyplot.xscale(222)
+	#pyplot.yscale(222)
+	pyplot.title("title string")
+	pyplot.xlabel("label string")
+	pyplot.ylabel("label, string")
+	pyplot.plot(2,3)
+	pyplot.show()
+
+mat()
